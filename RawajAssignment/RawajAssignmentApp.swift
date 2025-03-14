63cdf39df6f2572f8e9355c4f408d27d3bba0a17
Ahmed Ezzat <Ahmed.ezzat@mdi.com.eg>
2025-03-15 00:50:52 +0200

    Initial Commit

---
+++ RawajAssignment/RawajAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawajAssignmentApp.swift
+//  RawajAssignment
+//
+//  Created by Ahmed Ezzat on 15/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct RawajAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
